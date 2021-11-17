9fe878848fb2bf40ca1a640c063cfc9aa13e06da
Rcodeboss <morejonrafa@gmail.com>
2021-11-17 21:55:39 +0000

    more updates

--- src/models.py
+++ src/models.py
@@ -15,19 +15,27 @@ class Person(Base):
     id = Column(Integer, primary_key=True)
     name = Column(String(250), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
-    # Here we define columns for the table address.
-    # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
 
-    def to_dict(self):
-        return {}
+#class Person(Base):
+#    __tablename__ = 'person'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+#    id = Column(Integer, primary_key=True)
+#    name = Column(String(250), nullable=False)
+
+#class Address(Base):
+#    __tablename__ = 'address'
+#    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+#    id = Column(Integer, primary_key=True)
+#    street_name = Column(String(250))
+#    street_number = Column(String(250))
+#    post_code = Column(String(250), nullable=False)
+#    person_id = Column(Integer, ForeignKey('person.id'))
+#    person = relationship(Person)
+
+#    def to_dict(self):
+#        return {}
 
 ## Draw from SQLAlchemy base
 try:
